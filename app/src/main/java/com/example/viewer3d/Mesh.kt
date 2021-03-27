dc72353acc4a81b8a90e5109341475cda2dd2bab
Reynardo Perez <reynardo19@hotmail.com>
2021-03-26 22:07:48 -0400

    code refactor

--- app/src/main/java/com/example/viewer3d/Mesh.kt
+++ app/src/main/java/com/example/viewer3d/Mesh.kt
@@ -1,8 +1,52 @@
 package com.example.viewer3d
 
-class Mesh(vertices : FloatArray, indices: ByteArray) {
+import android.opengl.GLES20.*
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+import java.nio.FloatBuffer
 
-     public fun Init(){
+const val FLOAT_BYTES = 4
 
+class Mesh(vertices : FloatArray, indices: ByteArray?) {
+
+
+     private var vertexBuffer: FloatBuffer
+     private val vertexStride: Int = COORDS_PER_VERTEX * FLOAT_BYTES
+
+     init {
+          vertexBuffer = ByteBuffer.allocateDirect(vertices.size * FLOAT_BYTES).run {
+
+               // use the device hardware's native byte order
+               order(ByteOrder.nativeOrder())
+
+               // create a floating point buffer from the ByteBuffer
+               asFloatBuffer().apply {
+
+                    // add the coordinates to the FloatBuffer
+                    put(vertices)
+
+                    // set the buffer to read the first coordinate
+                    position(0)
+               }
+          }
+     }
+
+     public fun Bind_Test(program: Int){
+
+          var positionHandle = glGetAttribLocation(program, "vPosition").also {
+
+               // Enable a handle to the triangle vertices
+               glEnableVertexAttribArray(it)
+
+               // Prepare the triangle coordinate data
+               glVertexAttribPointer(
+                    it,
+                    COORDS_PER_VERTEX,
+                    GL_FLOAT,
+                    false,
+                    vertexStride,
+                    vertexBuffer
+               )
+          }
      }
 }
--- app/src/main/java/com/example/viewer3d/OpenGlRenderer.kt
+++ app/src/main/java/com/example/viewer3d/OpenGlRenderer.kt
@@ -10,7 +10,6 @@ import javax.microedition.khronos.opengles.GL10
 const val COORDS_PER_VERTEX = 2
 
 class OpenGlRenderer : GLSurfaceView.Renderer {
-    var verticesCount : Int = 0
 
     private val vertexShaderCode =
         "attribute vec4 vPosition;" +
@@ -19,126 +18,24 @@ class OpenGlRenderer : GLSurfaceView.Renderer {
                 "}"
 
     private val fragmentShaderCode =
-        "precision mediump float;" +
-                "uniform vec4 vColor;" +
-                "void main() {" +
-                "  gl_FragColor = vec4(1.0);" +
-                "}"
 
-    var program = 0
-    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
-
-        Tutorial();
-    }
-    var triangleCoords = floatArrayOf(     // in counterclockwise order:
-        0.0f, 0.622008459f,      // top
-        -0.5f, -0.311004243f,    // bottom left
-        0.5f, -0.311004243f     // bottom right
+    var triangleCoords = floatArrayOf(// in counterclockwise order:
+        -1.0f, 0.0f,      // top
+        0.0f, 1.0f,    // bottom left
+        1.0f, 0.0f     // bottom right
     )
 
-    lateinit var vertexBuffer:FloatBuffer
 
-    fun Tutorial() {
+    override fun onSurfaceCreated(gl: GL10?, config: EGLConfig?) {
 
-//        vertexBuffer =
-//                // (number of coordinate values * 4 bytes per float)
-//            ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
-//                // use the device hardware's native byte order
-//                order(ByteOrder.nativeOrder())
-//
-//                // create a floating point buffer from the ByteBuffer
-//                asFloatBuffer().apply {
-//                    // add the coordinates to the FloatBuffer
-//                    put(triangleCoords)
-//                    // set the buffer to read the first coordinate
-//                    position(0)
-//                }
-//            }
-
-                // (number of coordinate values * 4 bytes per float)
-
-        vertexBuffer = ByteBuffer.allocateDirect(triangleCoords.size * 4).run {
-                // use the device hardware's native byte order
-                order(ByteOrder.nativeOrder())
-
-                // create a floating point buffer from the ByteBuffer
-                asFloatBuffer().apply {
-                    // add the coordinates to the FloatBuffer
-                    put(triangleCoords)
-                    // set the buffer to read the first coordinate
-                    position(0)
-                }
-            }
-
-        val vertexShader: Int = loadShader(GL_VERTEX_SHADER, vertexShaderCode)
-        val fragmentShader: Int = loadShader(GL_FRAGMENT_SHADER, fragmentShaderCode)
-
-        // create empty OpenGL ES Program
-        program = glCreateProgram().also {
-
-            // add the vertex shader to program
-            glAttachShader(it, vertexShader)
-
-            // add the fragment shader to program
-            glAttachShader(it, fragmentShader)
-
-            // creates OpenGL ES program executables
-            glLinkProgram(it)
-        }
+        var mesh = Mesh(triangleCoords, null)
+        var shader = Shader(vertexShaderCode, fragmentShaderCode)
+        var program = shader.Bind()
+        mesh.Bind_Test(program)
     }
 
-    fun loadShader(type: Int, shaderCode: String): Int {
-
-        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
-        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
-        return glCreateShader(type).also { shader ->
-
-            // add the source code to the shader and compile it
-            glShaderSource(shader, shaderCode)
-            glCompileShader(shader)
-        }
-    }
-
-    private var positionHandle: Int = 0
-    private var mColorHandle: Int = 0
 
     private val vertexCount: Int = triangleCoords.size / COORDS_PER_VERTEX
-    private val vertexStride: Int = COORDS_PER_VERTEX * 4 // 4 bytes per vertex
-
-    fun draw() {
-        // Add program to OpenGL ES environment
-        glUseProgram(program)
-
-        // get handle to vertex shader's vPosition member
-        positionHandle = glGetAttribLocation(program, "vPosition").also {
-
-            // Enable a handle to the triangle vertices
-            glEnableVertexAttribArray(it)
-
-            // Prepare the triangle coordinate data
-            glVertexAttribPointer(
-                it,
-                COORDS_PER_VERTEX,
-                GL_FLOAT,
-                false,
-                vertexStride,
-                vertexBuffer
-            )
-
-            // get handle to fragment shader's vColor member
-//            mColorHandle = glGetUniformLocation(program, "vColor").also { colorHandle ->
-//
-//                // Set color for drawing the triangle
-//                glUniform4fv(colorHandle, 1, color, 0)
-//            }
-
-            // Draw the triangle
-            glDrawArrays(GL_TRIANGLES, 0, vertexCount)
-
-            // Disable vertex array
-            glDisableVertexAttribArray(it)
-        }
-    }
 
     override fun onSurfaceChanged(gl: GL10?, width: Int, height: Int) {
 
@@ -147,6 +44,8 @@ class OpenGlRenderer : GLSurfaceView.Renderer {
     override fun onDrawFrame(gl: GL10?) {
         glClear(GL_DEPTH_BUFFER_BIT or GL_COLOR_BUFFER_BIT)
 
-        draw();
+        glDrawArrays(GL_TRIANGLES, 0, vertexCount)
+
+        //glDisableVertexAttribArray(it)
     }
 }
