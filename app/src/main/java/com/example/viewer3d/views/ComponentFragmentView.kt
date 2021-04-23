66940c5b10c3f3178fbf3ebeb105e5da8065d08e
Reynardo Perez <reynardo19@hotmail.com>
2021-04-22 23:22:38 -0400

    mesh renderer component wip

--- app/src/main/java/com/example/viewer3d/views/ComponentFragmentView.kt
+++ app/src/main/java/com/example/viewer3d/views/ComponentFragmentView.kt
@@ -3,5 +3,5 @@ package com.example.viewer3d.views
 import androidx.fragment.app.Fragment
 import com.example.viewer3d.R
 
-class ComponentFragmentView : Fragment(R.layout.transform_fragment_view) {
+class ComponentFragmentView : Fragment(R.layout.meshrenderer_fragment_view) {
 }
