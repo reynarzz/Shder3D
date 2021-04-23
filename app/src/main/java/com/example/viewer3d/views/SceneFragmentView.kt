e08f65eb67ecc7f435d067a7715eb2cb1a7778aa
Reynardo Perez <reynardo19@hotmail.com>
2021-04-22 20:36:54 -0400

    component view

--- app/src/main/java/com/example/viewer3d/views/SceneFragmentView.kt
+++ app/src/main/java/com/example/viewer3d/views/SceneFragmentView.kt
@@ -3,5 +3,5 @@ package com.example.viewer3d.views
 import androidx.fragment.app.Fragment
 import com.example.viewer3d.R
 
-class SceneViewFragment : Fragment(R.layout.scene_view_fragment) {
+class SceneFragmentView : Fragment(R.layout.component_view) {
 }
