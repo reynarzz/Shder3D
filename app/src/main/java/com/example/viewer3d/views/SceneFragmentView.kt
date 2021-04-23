8f3cd43f122af3d206d3c1c547d9a323f887e8b7
Reynardo Perez <reynardo19@hotmail.com>
2021-04-23 02:05:01 -0400

    change app name

--- app/src/main/java/com/example/viewer3d/views/SceneFragmentView.kt
+++ app/src/main/java/com/example/viewer3d/views/SceneFragmentView.kt
@@ -3,5 +3,5 @@ package com.example.viewer3d.views
 import androidx.fragment.app.Fragment
 import com.example.viewer3d.R
 
-class SceneFragmentView : Fragment(R.layout.component_view) {
+class SceneFragmentView : Fragment(R.layout.scene_view_fragment) {
 }
